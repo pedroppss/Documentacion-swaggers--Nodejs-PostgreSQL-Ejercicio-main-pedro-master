315e1286661edbb14953500d863c97bef7dbc9c1
pedroppss <realmadrid19991@gmail.com>
2023-03-20 10:12:40 +0100

    Gestion de usuarios =>  nodejs con postgreSQL

---
+++ app/models/indexuser.js
@@ -0,0 +1,19 @@
+
+//const {Sequelize, DataTypes} = require('sequelize')
+//const sequelize = new Sequelize(`postgres://postgres:pps@gmail.com:5432/users`, {dialect: "postgres"})
+
+  //  sequelize.authenticate().then(() => {
+    //    console.log(`Database connected to discover`)
+    //}).catch((err) => {
+      //  console.log(err)
+    //})
+
+    //const db = {}
+    //db.Sequelize = Sequelize;
+    //db.sequelize = sequelize;
+
+
+//db.users = require('./departamentosuser.model.js') (sequelize, DataTypes);
+
+
+//module.exports = db;
