fd5b2d100eb30079ec77c748a392d8c27689ec7b
pedroppss <realmadrid19991@gmail.com>
2023-05-19 12:09:50 +0200

    -

--- app/Controllers/userController.js
+++ app/Controllers/userController.js
@@ -8,21 +8,17 @@ const User = db.users;
 //hash the users password before it is saved to the database with bcrypt
 const signup = async (req, res) => {
   try {
-    const { userName, email, password } = req.body;
+    const { name, email, password, role } = req.body;
     const data = {
-      userName,
+      name,
       email,
+      role,
       password: await bcrypt.hash(password, 10),
     };
     //saving the user
     const user = await User.create(data);
-
-
-    //if user details are captured
-    //generate token with user id and secret key in env file
-    // set cookie with generated token
     if (user) {
-      let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+      let token = jwt.sign({ id: user.id, role: user.role}, process.env.secretKey, {
         expiresIn: 1 * 24 * 60 * 60 * 1000,
       });
 
@@ -30,12 +26,13 @@ const signup = async (req, res) => {
       console.log("user", JSON.stringify(user, null, 2));
       console.log(token);
       //Send user details
-      return res.status(201).send(user);
+      return res.status(201).send({ user, token, message: "the user has been inserted successfully" });
     } else {
       return res.status(409).send("details are not correct");
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ success: false, message: error });
   }
 };
 
@@ -44,32 +41,33 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { name, password } = req.body;
 
     //Find a user by their email
     const user = await User.findOne({
       where: {
-        email: email
+        name:name,
       }
 
     });
 
-    //if the user's email is found, check the password against bcrypt
+    //if the user's name is found, check the password against bcrypt
     if (user) {
       const isSame = await bcrypt.compare(password, user.password);
 
       //if password is same generate token with user id and secret key in env file
 
       if (isSame) {
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+        let token = jwt.sign({ id: user.id, role: user.role}, process.env.secretKey, {
           expiresIn: 1 * 24 * 60 * 60 * 1000,
         });
 
         //if the password matches the one in the database, go ahead and generate a cookie for the userres.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
         console.log("user", JSON.stringify(user, null, 2));
         console.log(token);
+        
         //send user data
-        return res.status(201).send(user);
+        return res.status(201).json({ user, token, message: "Authentication success" });
       } else {
         return res.status(401).send("failed authentication");
       }
@@ -80,9 +78,42 @@ const login = async (req, res) => {
     console.log(error);
   }
 };
+const visualize = async (req, res) => {
+  const name = req.query.name;
+  const condition = name ? { name: { [Op.iLike]: `%${name}%` } } : null;
+  const result = await User.findAll({
+    where: condition
+  }).catch(err => {
+    res.status(500).json({ message: "error when displaying" });
+  });
+  res.status(201).json({ result, success: true, message: "Authorized success" })
+};
+const userDelete = async (req, res) => {
+  try {
+    const name = req.query;
+    await User.destroy({
+      where: {
+        name: req.query.name
+      }
+    }).then(num => {
+      if (num == 1) {
+        res.status(201).json({ success: true, message: "the user has been successfully deleted" });
+      } else {
+        res.status(409).json({ success: false, message: "user not found" });
+      }
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: err })
+  }
+
+};
 
 module.exports = {
   signup,
   login,
+  visualize,
+  userDelete,
+  
 };
 
--- app/middlewares/userAuth.js
+++ app/middlewares/userAuth.js
@@ -1,42 +1,132 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const db = require("../models");
+const { Pool } = require("pg");
+
 //Assigning db.users to User variable
 const User = db.users;
 
 //Function to check if username or email already exist in the database
 //this is to avoid having two users with the same username and email
 const saveUser = async (req, res, next) => {
-  //search the database to see if user exist
+
   try {
-    const username = await User.findOne({
+    const name = await User.findOne({
       where: {
-        userName: req.body.userName,
+        name: req.body.name,
       },
     });
     //if username exist in the database respond with a status of 409
-    if (username) {
-      return res.json(409).send("username already taken");
+    if (name) {
+      return res.json(409).send("name already taken");
     }
-
-    //checking if email already exist
-    const emailcheck = await User.findOne({
+    const pass = await User.findOne({
       where: {
-        email: req.body.email,
+        password: req.body.password,
       },
     });
-
-    //if email exist in the database respond with a status of 409
-    if (emailcheck) {
+    if (pass) {
       return res.json(409).send("Authentication failed");
     }
-
     next();
   } catch (error) {
     console.log(error);
   }
 };
+const authrole = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    if (!token) {
+      res.status(403).send({ message: "TOKEN NOT AUTHORIZED" });
+    } else {
+      jwt.verify(token, process.env.secretKey, function (err, payload) {
+        if (payload.role == "admin") {
+          next();
+        } else {
+          res.status(409).json({ message: "access failed, only administrators can access" });
+        }
+      })
+    }
+  } catch (err) {
+    return res.status(500).send({ success: false, message: "Invalid Token, not Authorized" });
+  }
+};
+const pool = new Pool({
+  host: "localhost",
+  user: "postgres",
+  password: "123456789",
+  database: "departments",
+  dialect: "postgres",
+  port: 5432,
+})
+const authroleuser = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    if (!token) {
+      res.status(403).send({ message: "TOKEN NOT AUTHORIZED" });
+    } else {
+      jwt.verify(token, process.env.secretKey, function (err, payload) {
+        if (payload.role == "admin") {
+          getUsers().then(response => {
+            res.status(201).json(response)
+          })
 
-//exporting module
+        } else if (payload.role == "user") {
+          getName().then(response => {
+            res.status(201).json(response)
+          })
+        }
+      })
+    }
+  } catch (err) {
+    return res.status(500).send({ success: false, message: "Invalid Token, not Authorized" });
+  }
+}
+const authroledepartment = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    if (!token) {
+      res.status(403).send({ message: "TOKEN NOT AUTHORIZED" });
+    } else {
+      jwt.verify(token, process.env.secretKey, function (err, payload) {
+        if (payload.role == "admin") {
+          getDepartament().then(response => {
+            res.status(201).json(response)
+          })
+
+        } else if (payload.role == "user") {
+          getDepartamenttitle().then(response => {
+            res.status(201).json(response)
+          })
+        }
+      })
+    }
+  } catch (err) {
+    return res.status(500).send({ success: false, message: "Invalid Token, not Authorized" });
+  }
+}
+
+
+const getUsers = async () => {
+  const response = await pool.query('SELECT * FROM  users');
+  return response.rows
+}
+const getName = async () => {
+  const response = await pool.query('SELECT name FROM users');
+  return response.rows
+}
+const getDepartament = async () => {
+  const response = await pool.query('SELECT * FROM departments');
+  return response.rows
+}
+const getDepartamenttitle = async () => {
+  const response = await pool.query('SELECT title FROM departments');
+  return response.rows
+}
+
 module.exports = {
   saveUser,
+  authrole,
+  authroleuser,
+  authroledepartment,
 };
